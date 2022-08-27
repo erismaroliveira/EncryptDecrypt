7700832494ac098a1622edf1c684991334c09d87
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-08-27 20:59:54 -0300

    Initial commit

---
+++ EncryptDecrypt/Controllers/EncryptDecryptController.cs
@@ -0,0 +1,24 @@
+﻿using EncryptDecrypt.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EncryptDecrypt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EncryptDecryptController : ControllerBase
+    {
+        [HttpGet, Route("Encrypt")]
+        public string Encrypt(string text)
+        {
+            var encryptString = EncryptDecryptManager.Encrypt(text);
+            return encryptString;
+        }
+
+        [HttpGet, Route("Decrypt")]
+        public string Decrypt(string text)
+        {
+            var decryptString = EncryptDecryptManager.Decrypt(text);
+            return decryptString;
+        }
+    }
+}
---
+++ EncryptDecrypt/Models/EncryptDecryptManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptDecrypt.Models
+{
+    public class EncryptDecryptManager
+    {
+        private readonly static string key = "erismardotnetdeveloper2022key123";
+
+        // AES Encryptor
+        public static string Encrypt(string text)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using(MemoryStream ms = new MemoryStream())
+                {
+                    using(CryptoStream cs = new CryptoStream((Stream)ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter str = new StreamWriter(cs))
+                        {
+                            str.Write(text);
+                        }
+                        array = ms.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        // AES Decryptor
+        public static string Decrypt(string text)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(text);
+
+            using(Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cs = new CryptoStream((Stream)ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(cs))
+                        {
+                            return sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
